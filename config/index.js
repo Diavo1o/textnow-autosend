1aedc766070b6f033b687f8a927ad0d3d6e5e874
Diavo1o <66171057+Diavo1o@users.noreply.github.com>
2021-02-14 23:15:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,4 +3,5 @@ module.exports = {
   password: process.env.TEXTNOW_PASSWORD,
   recipient: process.env.TEXTNOW_RECIPIENT,
   message: process.env.TEXTNOW_MESSAGE,
+  cookies_str: process.env.TEXTNOW_COOKIES
 };
